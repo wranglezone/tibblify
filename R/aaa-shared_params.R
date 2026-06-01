50ec277aa0d98687cd372f39c1a996d639501aab
Jon Harmon <jonthegeek@gmail.com>
2026-06-01 06:39:28 -0500

    Refine documentation of `fully_qualify`

--- R/aaa-shared_params.R
+++ R/aaa-shared_params.R
@@ -32,12 +32,12 @@
 #' @param .fill (`vector` or `NULL`) Optionally, a value to use if the field
 #'   is empty or does not exist. Note: this value must match the `.ptype_inner`
 #'   of the field (the value *before* any transformation), not the `.ptype`.
-#' @param fully_qualify (`logical(1)`) Should printed/formatted `tib_*()` and
-#'   `tspec_*()` calls be prefixed with `tibblify::`?
 #' @param force_names (`logical(1)`) Should names be printed even if they can be
 #'   deduced from the spec?
 #' @param .format (`character(1)` or `NULL`) Passed to the `format` argument of
 #'   [as.Date()].
+#' @param fully_qualify (`logical(1)`) Should `tib_*()` and `tspec_*()` calls be
+#'   prefixed with `tibblify::`?
 #' @param header_objects (`list`) A named list of header objects from an OpenAPI
 #'   spec.
 #' @param id_col (`character(1)`, `integer(1)`, or `symbol`) The column that
